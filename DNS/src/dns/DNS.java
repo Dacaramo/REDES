ee733335dcc0f0d91cfc1eca367c0cbedf74fa3d
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-04-30 15:57:07 -0500

    Incluí una función para extraer datos relevantes del query para armar una respuesta.

--- DNS/src/dns/DNS.java
+++ DNS/src/dns/DNS.java
@@ -22,12 +22,16 @@ public class DNS {
     public static void main(String[] args) throws IOException {
 
         try {
+            
             DatagramSocket socketUDP = new DatagramSocket(53);
-            byte[] bufer = new byte[1000];
+            
+            //Messages carried by UDP are restricted to 512 bytes (not counting the IP or UDP headers)
+            byte[] buffer = new byte[512];
 
             while (true) {
                 // Construimos el DatagramPacket para recibir peticiones
-                DatagramPacket peticion = new DatagramPacket(bufer, bufer.length);
+                //NOTA: El Datagram es un Datagrama UDP (tamaño de UDP Datagram)
+                DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
 
                 // Leemos una petición del DatagramSocket
                 socketUDP.receive(peticion);
@@ -38,6 +42,8 @@ public class DNS {
                         + peticion.getPort());
 
                 // Construimos el DatagramPacket para enviar la respuesta
+                
+                extraerData(peticion.getData());
                 
                 DatagramPacket respuesta = new DatagramPacket(peticion.getData(), peticion.getLength(), peticion.getAddress(), peticion.getPort());
                 
@@ -54,20 +60,90 @@ public class DNS {
     }
 
     private static byte[] CrearRespuesta(byte[] data) {
-        //TransactionID
         byte[] TrID = {0,0};
-        for (int i = 0; i<2; i++){
-            TrID[i] = data[i];
-        }
-        
         System.out.println("TransactionID");
-        for (int i = 0; i<2; i++){
-            System.out.println(TrID[i]);
+        int index = 0;
+        //TransactionID
+        while(index < 2){
+            TrID[index] = data[index];
+            System.out.println(TrID[index]);
+            index++;
         }
         
-        //banderas
+        //QR, OpCode & Banderas (sin RA)
         
+        //RA
+        //Z (Reserved for Future Use) = 4 bits en 0
+        //RCODE 0 = No Error
+        // 1, 2, 3, 4, 5...
+        byte razRcode;
+        
+        
+        //QDCOUNT
+        //ANCOUNT
+        //NSCOUNT
+        //ARCOUNT
         return TrID;
+    }
+    
+    private static void extraerData(byte[] data) {
+        byte[] TrID = {0,0};
+        System.out.println("TransactionID");
+        int index = 0;
+        //TransactionID
+        while(index < 2){
+            TrID[index] = data[index];
+            System.out.println(TrID[index]);
+            index++;
+        }
+        
+        //QR, OpCode & Banderas (sin RA)
+        byte thirdByte = data[index++];
+        //RA
+        //Z (Reserved for Future Use) = 4 bits en 0
+        //RCODE 0 = No Error
+        byte razRcode = data[index++];
+        
+        System.out.println(razRcode);
+        
+        //QDCOUNT
+        //TODO: Convertir a entero
+        byte[] qdCount = {0,0};
+        for(int i = 0; i < 2; i++){
+            qdCount[i] = data[index];
+            index++;
+        }
+        
+        //ANCOUNT
+        byte[] anCount = {0,0};
+        for(int i = 0; i < 2; i++){
+            anCount[i] = data[index];
+            index++;
+        }
+        
+        //NSCOUNT
+        byte[] nsCount = {0,0};
+        for(int i = 0; i < 2; i++){
+            nsCount[i] = data[index];
+            index++;
+        }
+        
+        //ARCOUNT
+        byte[] arCount = {0,0};
+        for(int i = 0; i < 2; i++){
+            arCount[i] = data[index];
+            index++;
+        }
+        
+        //Question section
+        //qdCountInt: qdCount en entero
+        //OJO: Solo se está tomando el segundo Byte (i.e. QDCOUNT máximo = 2^8)
+        int qdCountInt = qdCount[1];
+        for(int i = 0; i < qdCountInt; i++){
+            //QNAME
+            //QTYPE
+            //QCLASS
+        }
     }
 
 }
