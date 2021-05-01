962642d0ab7750fde8f93374b0b852606c6ed665
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-05-01 14:00:34 -0500

    Avances.

--- DNS/src/dns/DNS.java
+++ DNS/src/dns/DNS.java
@@ -3,13 +3,14 @@ package dns;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author USUARIO
  */
 public class DNS {
-
+        
     /**
      * @param args the command line arguments
      */
@@ -58,13 +59,14 @@ public class DNS {
                 System.out.println(" desde el puerto remoto: "
                         + peticion.getPort());
 
+                //queryResponseDataMap: HashMap de Campos compartidos entre Query y Response.
+                HashMap<String, String> queryResponseDataMap = new HashMap<String, String>();
+                ArrayList<byte[]> queryData = extraerData(peticion.getData(), queryResponseDataMap);
+                
                 // Construimos el DatagramPacket para enviar la respuesta
-                
-                extraerData(peticion.getData());
-                
                 DatagramPacket respuesta = new DatagramPacket(peticion.getData(), peticion.getLength(), peticion.getAddress(), peticion.getPort());
                 
-                //byte[] b = CrearRespuesta(peticion.getData());
+                ArrayList<Byte> respuestaBytes = crearRespuesta(queryResponseDataMap, registros, queryData);
                 // Enviamos la respuesta, que es un eco
                 socketUDP.send(respuesta);
             }
@@ -76,40 +78,68 @@ public class DNS {
         }
     }
 
-    private static byte[] CrearRespuesta(byte[] data) {
-        byte[] trID = {0,0};
-        System.out.println("TransactionID");
-        int index = 0;
-        //TransactionID
-        while(index < 2){
-            trID[index] = data[index];
-            System.out.println(trID[index]);
-            index++;
+    private static ArrayList<Byte> crearRespuesta(HashMap<String, String> queryResponseDataMap, ArrayList<ArrayList<String>> registros, ArrayList<byte[]> camposByte) {
+        
+        ArrayList<Byte> response = new ArrayList<Byte>();
+        String responseStr = "";
+        
+        String trID = queryResponseDataMap.get("ID");
+        String QR = "1";
+        String opCode = queryResponseDataMap.get("OPCODE");
+        String qdCount = queryResponseDataMap.get("QDCOUNT");
+        String qName = queryResponseDataMap.get("QNAME");
+        
+        ArrayList<ArrayList<String>> foundName = findInMasterFile(qName, registros);
+        Integer anCount = foundName.size();
+        String anCountStr = String.format("%16s", Integer.toBinaryString(anCount)).replace(' ', '0');
+        
+        String AA = "1";
+        String TC = "0";
+        String RD = "1";
+        String RA = "0";
+        String Z = "000";
+        String RCODE = "0000";
+        String NSCOUNT = "0000000000000000";
+        
+        responseStr += trID;
+        System.out.println(responseStr);
+        System.out.println(responseStr.length());
+        
+        responseStr += QR + opCode + AA + TC + RD;
+        System.out.println(responseStr);
+        System.out.println("OPCODE: " + opCode + opCode.length());
+        System.out.println(responseStr.length());
+        
+        responseStr += RA + Z + RCODE;
+        System.out.println(responseStr);
+        System.out.println(responseStr.length());
+        
+        responseStr += qdCount;
+        System.out.println(responseStr);
+        System.out.println(responseStr.length());
+        
+        responseStr += anCountStr;
+        System.out.println(responseStr);
+        System.out.println(responseStr.length());
+        
+        //NSCOUNT + ARCOUNT (Idéntico a NSCOUNT)
+        responseStr += NSCOUNT + NSCOUNT;
+        
+        for(int i = 0; i < responseStr.length(); i += 8){
+            Integer octet = Integer.parseInt(responseStr.substring(i, i + 8), 2);
+            byte octetByte = octet.byteValue();
+            response.add(octetByte);
         }
         
-        //QR, OpCode & Banderas (sin RA)
-        
-        //RA
-        //Z (Reserved for Future Use) = 4 bits en 0
-        //RCODE 0 = No Error
-        // 1, 2, 3, 4, 5...
-        byte razRcode;
-        
-        
-        //QDCOUNT
-        //ANCOUNT
-        //NSCOUNT
-        //ARCOUNT
-        return trID;
+        //Añadiendo QNAME, QTYPE y QCLASS del camposByte
+        response.add(camposByte[7]);
+        response.add(camposByte[8]);
+        response.add(camposByte[9]);
     }
     
-    private static ArrayList<byte[]> extraerData(byte[] data) {
+    private static ArrayList<byte[]> extraerData(byte[] data, HashMap<String, String> queryResponseDataMap) {
         //ArrayList where query data is stored
         ArrayList<byte[]> queryData = new ArrayList<byte[]>();
-        ArrayList<String> queryDataStr = new ArrayList<String>();
-        
-        //TransactionID
-        System.out.println("Transaction ID:");
         
         byte[] trID = {0,0};
         int index = 0;
@@ -120,23 +150,20 @@ public class DNS {
             index++;
         }
         
-        queryData.add(trID);
-        queryDataStr.add(trIDStr);
-        
-        System.out.println(trIDStr);
-        
         //QR, OpCode & Banderas (sin RA)
         byte[] thirdByte = {0};
         thirdByte[0] = data[index++];
-        queryData.add(thirdByte);
-        
+
+        //Extrayendo OPCode (campo compartido entre Query y Respuesta)
+        String thirdByteStr = toBits(thirdByte[0]);
+        String opCode = thirdByteStr.substring(1, 5);
+
         //RA
         //Z (Reserved for Future Use) = 4 bits en 0
         //RCODE 0 = No Error
         byte[] razRcode = {0};
         razRcode[0] = data[index++];
-        queryData.add(razRcode);
-        
+
         //QDCOUNT
         String qdCountStr = "";
         byte[] qdCount = {0,0};
@@ -145,33 +172,37 @@ public class DNS {
             qdCountStr += toBits(data[index]);
             index++;
         }
-        queryData.add(qdCount);
-        queryDataStr.add(qdCountStr);
-        
+
         //ANCOUNT
         byte[] anCount = {0,0};
         for(int i = 0; i < 2; i++){
             anCount[i] = data[index];
             index++;
         }
-        queryData.add(anCount);
-        
+
         //NSCOUNT
         byte[] nsCount = {0,0};
         for(int i = 0; i < 2; i++){
             nsCount[i] = data[index];
             index++;
         }
-        queryData.add(nsCount);
-        
+
         //ARCOUNT
         byte[] arCount = {0,0};
         for(int i = 0; i < 2; i++){
             arCount[i] = data[index];
             index++;
         }
-        queryData.add(arCount);
         
+        //Armando el ArrayList de byte[]...
+        queryData.add(trID);
+        queryData.add(thirdByte);
+        queryData.add(razRcode);
+        queryData.add(qdCount);
+        queryData.add(anCount);
+        queryData.add(nsCount);
+        queryData.add(arCount);
+
         //Question section
         //qdCountInt: qdCount en entero
         int qdCountInt = Integer.parseInt(qdCountStr, 2);
@@ -187,31 +218,52 @@ public class DNS {
             //String donde se va a almacenar el nombre de dominio
             String name = "";
             
+            byte[] qNameByte = {0,0,0,0,0,0,0,0,0,0,0};
+            int j = 0;
             while(data[index] != 0){
                 
-                //La siguiente línea es de prueba...
-                System.out.println(index);
-                
+                qNameByte[j++] = data[index];
                 q_name_lengthStr = toBits(data[index++]);
                 labelLength = Integer.parseInt(q_name_lengthStr, 2);
+                
+                //Prueba
+                //System.out.println(labelLength);
                 
                 byte[] byteName = new byte[labelLength];
                 for(int k = 0; k < labelLength; k++){
                     byteName[k] = data[index];
+                    qNameByte[j++] = byteName[k];
                     index++;
                 }
-                
-                //La siguiente línea es de prueba...
-                System.out.println(new String(byteName));
-                
-                //index++;
+
                 //Label copiado al nombre
                 name += new String(byteName) + '.';
             }
             name = name.substring(0, name.length()-1);
-            //La siguiente línea es de prueba...
-            System.out.println(name);
             
+            //PRUEBA
+            //System.out.println(qNameByte);
+            
+            //QNAME index = 7
+            queryData.add(qNameByte);
+            
+            //QTYPE index = 8
+            byte[] qType = {0,0};
+            qType[0] = data[index++];
+            qType[1] = data[index++];
+            queryData.add(qType);
+            
+            //QCLASS index 10
+            byte[] qClass = {0,0};
+            qClass[0] = data[index++];
+            qClass[1] = data[index++];
+            queryData.add(qClass);
+            
+            queryResponseDataMap.put("ID", trIDStr);
+            queryResponseDataMap.put("OPCODE", opCode);
+            queryResponseDataMap.put("QDCOUNT", qdCountStr);
+            queryResponseDataMap.put("QNAME", name);
+
             //QTYPE
             //QCLASS
         }
@@ -229,12 +281,15 @@ public class DNS {
         return result.toString();
     }
     
-    private static ArrayList<String> findInMasterFile(String name, ArrayList<ArrayList<String>> registros){
+    private static ArrayList<ArrayList<String>> findInMasterFile(String name, ArrayList<ArrayList<String>> registros){
+        ArrayList<ArrayList<String>> registrosNombre = new ArrayList<ArrayList<String>>();
         for(ArrayList<String> row : registros){
             if(row.get(0).compareTo(name) == 0){
-                return row;
+                registrosNombre.add(row);
             }
         }
+        if(!registrosNombre.isEmpty())
+            return registrosNombre;
         return null;
     }
 }
